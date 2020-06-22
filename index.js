217d0eea7fb94cd70344998ebd96eaa251bcac67
thiagomedina <thiagomedina001@gmail.com>
2020-06-22 19:18:23 -0300

    up

--- index.js
+++ index.js
@@ -8,19 +8,33 @@ const fetchData = async (url) => {
 
 const main =  async () => {
     
-    const content = await fetchData('http://www.finep.gov.br/chamadas-publicas?situacao=aberta');
+    const content = await fetchData('http://www.finep.gov.br/chamadas-publicas?situacao=aberta');   
     const $ = cheerio.load(content);
+     let arr = [];
+    $('#conteudoChamada .item').each((i, e) => {
+        const element = $(e);   
+        const tema = element.find(' .tema.div > span:Contains("Tecnologia")').text();
+        if(tema){
+           const url = element.find('h3 > a').attr('href')
+           arr.push({url});
 
-    let temasArray = [];
-    $('div.conteudoChamada').each((i, e) => {
+        }
 
-        const tema = $(e).find('.item > h3').attr('href');
-        const data = {tema};
         
-        temasArray.push(data);
     });
-    console.log(temasArray)
-
+    return arr;
 };
 
-main();
+const teste = async () =>{
+  const url =  await main();
+  let dados = [];
+  const content = url.map( async (e) =>{
+      const teste = await fetchData(e.url);
+      const $ = cheerio.load(teste);
+      let titulo = $('tit_pag > a').text();
+      dados.push({titulo});
+
+
+  })
+}
+teste();
