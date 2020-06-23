945c0e1504dce7af75a0d9bdc200db437ceb6f16
thiagomedina <thiagomedina001@gmail.com>
2020-06-22 22:17:22 -0300

    index

--- index.js
+++ index.js
@@ -6,35 +6,33 @@ const fetchData = async (url) => {
     return result.data;
 }
 
-const main =  async () => {
-    
-    const content = await fetchData('http://www.finep.gov.br/chamadas-publicas?situacao=aberta');   
-    const $ = cheerio.load(content);
-     let arr = [];
-    $('#conteudoChamada .item').each((i, e) => {
-        const element = $(e);   
-        const tema = element.find(' .tema.div > span:Contains("Tecnologia")').text();
-        if(tema){
-           const url = element.find('h3 > a').attr('href')
-           arr.push({url});
+const main = async () => {
 
-        }
+    const content = await fetchData('http://www.finep.gov.br/chamadas-publicas?situacao=aberta');
+    let  $ = cheerio.load(content);
+    let arr = [];
+    $('#conteudoChamada .item').each((i, e) => {
+        const element = $(e);
+        const tema = element.find(' .tema.div > span:Contains("Tecnologia")').text();
+       
+        if (tema) {
+            const url = 'http://www.finep.gov.br' + element.find('h3 > a').attr('href')
+            arr.push({url})
+                 var res = await fetchData(url)
+            
+
+             $ = cheerio.load(res)
+             console.log($)
+
+            }
+       
 
         
+
     });
-    return arr;
+
+
 };
 
-const teste = async () =>{
-  const url =  await main();
-  let dados = [];
-  const content = url.map( async (e) =>{
-      const teste = await fetchData(e.url);
-      const $ = cheerio.load(teste);
-      let titulo = $('tit_pag > a').text();
-      dados.push({titulo});
 
-
-  })
-}
-teste();
+main();
